4b8e981b89c6fa0b9ac2d49ec05aae7adbb8514a
Mariusz Karpiarz <mariusz.karpiarz@datacentred.co.uk>
2017-05-10 21:31:07 +0100

    Get a list of servers and prepare to migrate.

--- live_migrate_all_instances.py
+++ live_migrate_all_instances.py
@@ -21,6 +21,10 @@ def get_nova_creds():
     d['project_id'] = os.environ['OS_PROJECT_NAME']
     return d
 
+def get_hostname_of_host(server):
+    #return getattr(server, 'OS-EXT-SRV-ATTR:host')
+    return getattr(server, 'OS-EXT-SRV-ATTR:hypervisor_hostname')
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--source", help="the source hypervisor")
@@ -28,7 +32,7 @@ def main():
     args = parser.parse_args()
     host_source = args.source
     host_target = args.target
-    print("Will live migrate all instances from %s to %s" % (host_source, host_target) )
+    print("INFO: This will sequentially live migrate all ACTIVE instances from %s to %s" % (host_source, host_target) )
 
     auth = v3.Password(auth_url='https://compute.datacentred.io:5000/v3/',
                        username=os.environ['OS_USERNAME'],
@@ -38,9 +42,25 @@ def main():
                        project_domain_id='default')
     sess = session.Session(auth=auth)
     nova = client.Client("2.1", session=sess)
-    print nova.hypervisors.list()
-    print nova.servers.list()
-    print nova.hosts.list()
+    #print(nova.hypervisors.list())
+    #print(nova.servers.list())
+    servers_to_migrate = nova.hypervisors.search(host_source, servers=True)[0].servers
+    for s in servers_to_migrate:
+        server = nova.servers.get( s['uuid'] )
+        server_id = server.id
+        server_name = server.name
+        server_status = server.status
+        server_host = get_hostname_of_host(server)
+        # or faster but potentially more error prone:
+        #server_host = host_source
+        if server_status == 'ACTIVE':
+            print("INFO: Original host: %s" % (server_host)
+            print("INFO: Live-migrating server \'%s\' (UUID: %s)" % (server_name, server_id))
+            print("INFO: Current host: %s" % get_hostname_of_host(server) )
+            print("INFO: Current status: %s" % server.status)
+            print("INFO: Progress: %s" % server.progress)
+        else:
+            print("WARNING: Skipping server \'%s\' (UUID: %s) because it's in the state: %s" % (server_name, server_id, server_status) )
 
 if __name__ == "__main__":
     main()
